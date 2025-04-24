3b1e1f172388767b7c4cde12b52b7f901131fc6a
ImedSIO <144783338+ImedSIO@users.noreply.github.com>
2025-04-24 09:34:43 +0200

    Update Synthese.js

--- src/components/Synthese/Synthese.js
+++ src/components/Synthese/Synthese.js
@@ -13,12 +13,12 @@ function Synthese() {
         <p style={{ color: "white" }}>
           Tableau récapitulatif de mes différents projets et compétences.
         </p>
-        
+
         <Row className="mb-5">
           <Col md={12}>
             <Button 
               variant="primary" 
-              href="https://docs.google.com/spreadsheets/d/e/https://simaoui-imed.vercel.app/pub?output=xlsx"
+              href="https://docs.google.com/spreadsheets/d/1z0Vo4pwHswU5HSVdQd7vpCfhS-O6vav7auKyD6Nnpiw/export?format=xlsx"
               target="_blank"
               rel="noopener noreferrer"
             >
@@ -26,13 +26,14 @@ function Synthese() {
             </Button>
           </Col>
         </Row>
-        
+
         <Row>
           <Col md={12} style={{ height: "600px" }}>
             <iframe
-              src="https://docs.google.com/spreadsheets/d/e/https://simaoui-imed.vercel.app/pubhtml?widget=true&amp;headers=false"
+              src="https://docs.google.com/spreadsheets/d/e/2PACX-1vQgzYKViP1rEx-bHvka_tJd36e0W3Up-0UX5lns_Acd7a6bIq9IBRWGbZkPC5ZQIw/pubhtml?widget=true&amp;headers=false"
               style={{ width: "100%", height: "100%", border: "none" }}
               title="Tableau synthèse"
+              allowFullScreen
             />
           </Col>
         </Row>
