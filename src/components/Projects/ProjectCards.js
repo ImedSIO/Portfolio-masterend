5c497a184347ff66000f3f52e7588554a64c4123
ImedSIO <144783338+ImedSIO@users.noreply.github.com>
2025-04-24 13:00:15 +0200

    Update ProjectCards.js

--- src/components/Projects/ProjectCards.js
+++ src/components/Projects/ProjectCards.js
@@ -8,50 +8,106 @@ import { FaInfoCircle } from "react-icons/fa";
 
 
 function ProjectCards(props) {
+  const [showModal, setShowModal] = useState(false);
+
+  const handleClose = () => setShowModal(false);
+  const handleShow = () => setShowModal(true);
+
   return (
-    <Card className="project-card-view">
-      <Card.Img variant="top" src={props.imgPath} alt="card-img" />
-      <Card.Body>
-        <Card.Title className="project-title">{props.title}</Card.Title>
-        <Card.Text style={{ textAlign: "justify" }} className="project-description">
-          {props.description}
-        </Card.Text>
-        <div className="project-buttons">
-          <Button
-            variant="primary"
-            href={props.ghLink}
-            target="_blank"
-            className="project-button"
-          >
-            <BsGithub /> &nbsp;
-            {props.isBlog ? "Blog" : "GitHub"}
-          </Button>
-         
-          {!props.isBlog && props.demoLink && (
+    <>
+      <Card className="project-card-view">
+        <Card.Img variant="top" src={props.imgPath} alt="card-img" />
+        <Card.Body>
+          <Card.Title className="project-title">{props.title}</Card.Title>
+          <Card.Text style={{ textAlign: "justify" }} className="project-description">
+            {props.description}
+          </Card.Text>
+          <div className="project-buttons">
             <Button
               variant="primary"
-              href={props.demoLink}
+              href={props.ghLink}
               target="_blank"
               className="project-button"
             >
-              <CgWebsite /> &nbsp;
-              {"Demo"}
+              <BsGithub /> &nbsp;
+              {props.isBlog ? "Blog" : "GitHub"}
+            </Button>
+           
+            {!props.isBlog && props.demoLink && (
+              <Button
+                variant="primary"
+                href={props.demoLink}
+                target="_blank"
+                className="project-button"
+              >
+                <CgWebsite /> &nbsp;
+                {"Demo"}
+              </Button>
+            )}
+           
+            {/* Bouton de détails qui ouvre la modal */}
+            <Button
+              variant="info"
+              onClick={handleShow}
+              className="project-button"
+            >
+              <FaInfoCircle /> &nbsp;
+              {"Détails"}
+            </Button>
+          </div>
+        </Card.Body>
+      </Card>
+
+      {/* Modal pour afficher les détails du projet */}
+      <Modal show={showModal} onHide={handleClose} size="lg" centered className="project-modal">
+        <Modal.Header closeButton>
+          <Modal.Title>{props.title}</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <img
+            src={props.imgPath}
+            alt={props.title}
+            className="img-fluid project-modal-img"
+          />
+          <h4 className="mt-4">Description</h4>
+          <p>{props.description}</p>
+         
+          <h4 className="mt-4">Technologies utilisées</h4>
+          <ul className="tech-list">
+            {props.technologies.map((tech, index) => (
+              <li key={index}>{tech}</li>
+            ))}
+          </ul>
+         
+          <h4 className="mt-4">Fonctionnalités principales</h4>
+          <ul>
+            {props.features.map((feature, index) => (
+              <li key={index}>{feature}</li>
+            ))}
+          </ul>
+         
+          {props.challenges && (
+            <>
+              <h4 className="mt-4">Défis rencontrés</h4>
+              <p>{props.challenges}</p>
+            </>
+          )}
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Fermer
+          </Button>
+          {props.demoLink && (
+            <Button variant="primary" href={props.demoLink} target="_blank">
+              <CgWebsite /> Voir la démo
             </Button>
           )}
-         
-          {/* Nouveau bouton de détails */}
-          <Button
-            variant="info"
-            href={props.detailsLink}
-            target="_blank"
-            className="project-button"
-          >
-            <FaInfoCircle /> &nbsp;
-            {"Détails"}
+          <Button variant="dark" href={props.ghLink} target="_blank">
+            <BsGithub /> Voir le code
           </Button>
-        </div>
-      </Card.Body>
-    </Card>
+        </Modal.Footer>
+      </Modal>
+    </>
   );
 }
 export default ProjectCards;
