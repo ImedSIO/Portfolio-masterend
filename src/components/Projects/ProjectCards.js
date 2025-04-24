df852b8e5a3e330189ae7bdb93faf69703b560f7
ImedSIO <144783338+ImedSIO@users.noreply.github.com>
2025-04-24 11:33:06 +0200

    Update ProjectCards.js

--- src/components/Projects/ProjectCards.js
+++ src/components/Projects/ProjectCards.js
@@ -9,30 +9,44 @@ function ProjectCards(props) {
     <Card className="project-card-view">
       <Card.Img variant="top" src={props.imgPath} alt="card-img" />
       <Card.Body>
-        <Card.Title>{props.title}</Card.Title>
-        <Card.Text style={{ textAlign: "justify" }}>
+        <Card.Title className="project-title">{props.title}</Card.Title>
+        <Card.Text style={{ textAlign: "justify" }} className="project-description">
           {props.description}
         </Card.Text>
-        <Button variant="primary" href={props.ghLink} target="_blank">
-          <BsGithub /> &nbsp;
-          {props.isBlog ? "Blog" : "GitHub"}
-        </Button>
-        {"\n"}
-        {"\n"}
-
-        {/* If the component contains Demo link and if it's not a Blog then, it will render the below component  */}
-
-        {!props.isBlog && props.demoLink && (
+        <div className="project-buttons">
           <Button
             variant="primary"
-            href={props.demoLink}
+            href={props.ghLink}
             target="_blank"
-            style={{ marginLeft: "10px" }}
+            className="project-button"
           >
-            <CgWebsite /> &nbsp;
-            {"Demo"}
+            <BsGithub /> &nbsp;
+            {props.isBlog ? "Blog" : "GitHub"}
           </Button>
-        )}
+         
+          {!props.isBlog && props.demoLink && (
+            <Button
+              variant="primary"
+              href={props.demoLink}
+              target="_blank"
+              className="project-button"
+            >
+              <CgWebsite /> &nbsp;
+              {"Demo"}
+            </Button>
+          )}
+         
+          {/* Nouveau bouton de détails */}
+          <Button
+            variant="info"
+            href={props.detailsLink}
+            target="_blank"
+            className="project-button"
+          >
+            <FaInfoCircle /> &nbsp;
+            {"Détails"}
+          </Button>
+        </div>
       </Card.Body>
     </Card>
   );
