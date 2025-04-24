f8e65906fc217150b6d7b6176dcab78e50e87595
ImedSIO <144783338+ImedSIO@users.noreply.github.com>
2025-04-24 13:06:44 +0200

    Update Projects.js

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -16,53 +16,7 @@ import { CgWebsite } from "react-icons/cg";
 import { BsGithub } from "react-icons/bs";
 import { FaInfoCircle } from "react-icons/fa";
 
-function ProjectCards(props) {
-  return (
-    <Card className="project-card-view">
-      <Card.Img variant="top" src={props.imgPath} alt="card-img" />
-      <Card.Body>
-        <Card.Title className="project-title">{props.title}</Card.Title>
-        <Card.Text style={{ textAlign: "justify" }} className="project-description">
-          {props.description}
-        </Card.Text>
-        <div className="project-buttons">
-          <Button
-            variant="primary"
-            href={props.ghLink}
-            target="_blank"
-            className="project-button"
-          >
-            <BsGithub /> &nbsp;
-            {props.isBlog ? "Blog" : "GitHub"}
-          </Button>
-         
-          {!props.isBlog && props.demoLink && (
-            <Button
-              variant="primary"
-              href={props.demoLink}
-              target="_blank"
-              className="project-button"
-            >
-              <CgWebsite /> &nbsp;
-              {"Demo"}
-            </Button>
-          )}
-         
-          {/* Nouveau bouton de détails */}
-          <Button
-            variant="info"
-            href={props.detailsLink}
-            target="_blank"
-            className="project-button"
-          >
-            <FaInfoCircle /> &nbsp;
-            {"Détails"}
-          </Button>
-        </div>
-      </Card.Body>
-    </Card>
-  );
-}
+
 
 function Projects() {
   // Données des projets avec informations détaillées
