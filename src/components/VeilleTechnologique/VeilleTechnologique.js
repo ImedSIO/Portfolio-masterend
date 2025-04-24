af89e91b9405ad50f6e2d60ba11f6587950befc3
LaKensak <150548098+LaKensak@users.noreply.github.com>
2025-04-24 22:18:34 +0200

    Update VeilleTechnologique.js

--- src/components/VeilleTechnologique/VeilleTechnologique.js
+++ src/components/VeilleTechnologique/VeilleTechnologique.js
@@ -1,8 +1,6 @@
 import React from "react";
-import { Container, Row, Col, Card } from "react-bootstrap";
 import Particle from "../Particle";
-// Importez le CSS (ajustez le chemin si nécessaire)
-import "../veilletechnologique.css";
+import "../../veilletechnologique.css";
 
 function VeilleTechnologique() {
   return (
