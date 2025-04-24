1539cfc8afa5c650e731adceb3ba459d6e398ba1
ImedSIO <144783338+ImedSIO@users.noreply.github.com>
2025-04-24 09:10:09 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import React, { useState, useEffect } from "react";
+import Preloader from "../src/components/Pre";
 import Navbar from "./components/Navbar";
 import Home from "./components/Home/Home";
 import About from "./components/About/About";
 import Projects from "./components/Projects/Projects";
-import Synthese from "./components/Synthese/Synthese"; // Import the new component
+import Footer from "./components/Footer";
 import Resume from "./components/Resume/ResumeNew";
+
 import {
   BrowserRouter as Router,
   Route,
   Routes,
   Navigate
 } from "react-router-dom";
+
 import ScrollToTop from "./components/ScrollToTop";
 import "./style.css";
 import "./App.css";
@@ -29,18 +32,18 @@ function App() {
 
   return (
     <Router>
+      <Preloader load={load} />
       <div className="App" id={load ? "no-scroll" : "scroll"}>
         <Navbar />
         <ScrollToTop />
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/a-propos" element={<About />} />
-          <Route path="/projets" element={<Projects />} />
-          <Route path="/synthese" element={<Synthese />} /> {/* Add this route */}
-          <Route path="/cv" element={<Resume />} />
-          <Route path="/blog" element={<Blog />} />
-          <Route path="*" element={<Navigate to="/"/>} />
+          <Route path="/project" element={<Projects />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/resume" element={<Resume />} />
+          <Route path="*" element={<Navigate to="/" />} />
         </Routes>
+        <Footer />
       </div>
     </Router>
   );
